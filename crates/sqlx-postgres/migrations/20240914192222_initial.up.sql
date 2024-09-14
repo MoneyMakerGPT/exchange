c1455ba0257feb93d50c53f2a558a65fd36dce15
Jogeshwar Singh <singhjogeshwar007@gmail.com>
2024-09-15 01:51:08 +0530

    feat: setup sqlx for postgres

---
+++ crates/sqlx-postgres/migrations/20240914192222_initial.up.sql
@@ -0,0 +1,9 @@
+-- Add up migration script here
+CREATE TABLE IF NOT EXISTS users (
+	id  BIGSERIAL PRIMARY KEY,
+	email       VARCHAR(200) NOT NULL,
+	first_name  VARCHAR(200) NOT NULL,
+	last_name   VARCHAR(200) NOT NULL,
+	username    VARCHAR(50) UNIQUE NOT NULL,
+	UNIQUE (username)
+);
